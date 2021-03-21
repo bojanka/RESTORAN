3e9e02a848c6a9df7241a6853b55ca47b5a2e5c7
dushkaa <79053001+dushkaa@users.noreply.github.com>
2021-03-21 15:54:15 +0100

    Update Restoran.java
    Radile smo zajedno preko Discorda.

--- Restoran.java
+++ Restoran.java
@@ -1,33 +1,33 @@
 class Namirnica{
     private String naziv;
-    private double cena;
 
     String getNaziv(){return naziv;}
-    double getCena(){return cena;}
 
     void setNaziv(String naziv){this.naziv = naziv;}
-    void setCena(double cena){this.cena = cena;}
 
-    Namirnica(String naziv, double cena){
+    Namirnica(String naziv){
         this.naziv = naziv;
-        this.cena = cena;
     }
 
     @Override
     public String toString() {
-        return "naziv namirnice: " + naziv + ", cena namirnice: " + cena;
+        return "naziv namirnice: " + naziv;
     }
 
 }
 class Pice extends Namirnica{
     private boolean gaziranost;
+    private double cena;
 
     Pice(String naziv, double cena, boolean gaziranost)
     {
-        super(naziv, cena);
+        super(naziv);
+        this.cena=cena;
         this.gaziranost = gaziranost;
     }
-
+    String getCena(){return cena;}
+    void setCena(String cena){this.cena = cena;}
+    
     String getGaziranost(){
         if(gaziranost == true)
         {
@@ -59,13 +59,37 @@ class Jelo{
     }
 }
 class Meni{
-    private Jelo[] meni;
+    private ArrayList<Jelo> meni=new ArrayList<Jelo>();
+    public Meni(String fajl) throws IOException{
+        BufferedReader br=new BufferedReader(new FileReader(fajl));
+        String linija=br.readLine();
+        int i=0;
+        Jelo o=new Jelo();
+        while(linija != null)
+        {
+            int j=0;
+            String tokeni[]=linija.split(", ");
+            o.setNaziv(tokeni[0].trim());
+            o.setCena(Integer.parseInt(tokeni[1].trim()));
+            Namirnica potrebno[];
+            while((tokeni.length-2)>j){
+                potrebno[j]=tokeni[j+2].trim();
+            }
+            o.setNizNamirnica(potrebno);
+            meni.set(i, o);
+            linija=br.readLine();
+        }
+        br.close();
+    }
 
     void ispisiJela(){
-        for(int i = 0; i<meni.length ;i++)
-        {
-            System.out.println(meni[i].toString() + "\n");
-        }
+        for (Jelo jelo : meni)
+            {
+                System.out.println(jelo.toString() + "\n");
+            }
+    }
+    public void DodajJelo(Jelo jelo){
+        meni.add(jelo);
     }
     ///dodaj jelo,ispis jelo...
 }
@@ -77,4 +101,16 @@ class Racun{
     private Narudzbina n;
     ///metodu ispisuje sva jela u narudzbini
     ///i sabira cene i ispisuje zajednicku cenu
+}
+public static void main(String[] args) {
+    ArrayList<Namirnica> frizider=new ArrayList<Namirnica>();
+    BufferedReader br=new BufferedReader(new FileReader("frizider.txt"));
+        String linija=br.readLine();
+        String tokeni[]=linija.split(", ");
+        for(int i=0;i<tokeni.length;i++)
+        {
+            Namirnica n=new Namirnica(tokeni[i].trim());
+            frizider.add(n);
+        }
+        br.close();
 }
