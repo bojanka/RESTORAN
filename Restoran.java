eae67ef20cd9e389ad9150b57c4dbca60aabd315
bojanka <79052487+bojanka@users.noreply.github.com>
2021-05-09 21:02:38 +0200

    Update Restoran.java
    Meni u mainu i ispravke delova

--- Restoran.java
+++ Restoran.java
@@ -1,46 +1,49 @@
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-class Namirnica{
+class Namirnica {
     private String naziv;
 
-    String getNaziv(){return naziv;}
+    String getNaziv() {
+        return naziv;
+    }
 
-    void setNaziv(String naziv){this.naziv = naziv;}
-
-    Namirnica(String naziv){
+    void setNaziv(String naziv) {
         this.naziv = naziv;
     }
-    void kupi(Namirnica n)
-    {
-        ///upisivanjem u fajl dodajemo namirnicu u frizider
+
+    Namirnica(String naziv) {
+        this.naziv = naziv;
     }
 
     @Override
     public String toString() {
-        return "naziv namirnice: " + naziv ;
+        return "naziv namirnice: " + naziv;
     }
 
 }
-class Pice extends Namirnica{
-    private boolean gaziranost;
-    private double cena;
 
-    Pice(String naziv, double cena, boolean gaziranost)
-    {
+class Pice extends Namirnica {
+    private double cena;
+    private boolean gaziranost;
+
+    Pice(String naziv, double cena, boolean gaziranost) {
         super(naziv);
-        this.cena=cena;
+        this.cena = cena;
         this.gaziranost = gaziranost;
     }
-    double getCena(){return cena;}
 
-    String getGaziranost(){
-        if(gaziranost == true)
-        {
+    double getCena() {
+        return cena;
+    }
+
+    String getGaziranost() {
+        if (gaziranost == true) {
             return "DA";
-        }else
+        } else
             return "NE";
     }
 
@@ -49,123 +52,175 @@ class Pice extends Namirnica{
         return "naziv namirnice: " + getNaziv() + ", cena namirnice: " + getCena() + "gaziranost: " + getGaziranost();
     }
 }
-class Jelo{
+
+class Jelo {
     private String naziv;
     private double cena;
     private Namirnica[] potrebneNamirnice;
 
-    String getNaziv(){return naziv;}
-    double getCena(){return cena;}
+    String getNaziv() {
+        return naziv;
+    }
 
-    void setNaziv(String naziv){this.naziv = naziv;}
-    void setCena(double cena){this.cena = cena;}
-    void setNizNamirnica(Namirnica[] potrebneNamirnice){this.potrebneNamirnice = potrebneNamirnice;}
+    double getCena() {
+        return cena;
+    }
+
+    void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    void setNizNamirnica(Namirnica[] potrebneNamirnice) {
+        this.potrebneNamirnice = potrebneNamirnice;
+    }
 
     @Override
     public String toString() {
         return "Naziv jela: " + naziv + ", cena jela: " + cena;
     }
 }
-class Meni{
-    private ArrayList<Jelo> meni=new ArrayList<Jelo>();
-    public Meni(String fajl) throws IOException{
-        BufferedReader br=new BufferedReader(new FileReader(fajl));
-        String linija=br.readLine();
-        int i=0;
-        Jelo o=new Jelo();
-        while(linija != null)
-        {
-            int j=0;
-            String tokeni[]=linija.split(", ");
+
+class Meni {
+    private ArrayList<Jelo> meni = new ArrayList<Jelo>();
+
+    public Meni(String fajl) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fajl));
+        String linija = br.readLine();
+        int i = 0;
+        Jelo o = new Jelo();
+        while (linija != null) {
+            int j = 0;
+            String tokeni[] = linija.split(", ");
             o.setNaziv(tokeni[0].trim());
             o.setCena(Integer.parseInt(tokeni[1].trim()));
             Namirnica potrebno[];
-            while((tokeni.length-2)>j){
-                potrebno[j]=tokeni[j+2].trim();
+            while ((tokeni.length - 2) > j) {
+                potrebno[j].setNaziv(tokeni[j + 2].trim());
             }
             o.setNizNamirnica(potrebno);
             meni.set(i, o);
-            linija=br.readLine();
+            linija = br.readLine();
         }
         br.close();
     }
 
-    void ispisiJela(){
-        for (Jelo jelo : meni)
-            {
-                System.out.println(jelo.toString() + "\n");
-            }
-    }
-    public void DodajJelo(Jelo jelo){
-        meni.add(jelo);
-    }
-}
-class Narudzbina{
-    public ArrayList<Jelo> narucenaJela=new ArrayList<Jelo>();
-    public ArrayList<Pice> narucenaPica=new ArrayList<Pice>();
-    void naruciJelo(Jelo j)
-    {
-        narucenaJela.add(j);
-    }
-    void naruciPice(Jelo p)
-    {
-        narucenaJela.add(p);
-    }
-    void ispisiNarudzbinu()
-    {
-        for (Jelo jelo : narucenaJela )
-        {
+    void ispisiJela() {
+        for (Jelo jelo : meni) {
             System.out.println(jelo.toString() + "\n");
         }
-        for (Pice pice : narucenaPica)
-        {
+    }
+    /// potrebne namernice porediti sa onima u listi
+    /// iscitati iz fajla u listu sve namernice i proveravati da li za jelo koje je
+    /// naruceno imaju namirnice
+}
+
+class Narudzbina {
+    public ArrayList<Jelo> narucenaJela = new ArrayList<Jelo>();
+    public ArrayList<Pice> narucenaPica = new ArrayList<Pice>();
+
+    void naruciJelo(Jelo j) {
+        narucenaJela.add(j);
+    }
+
+    void naruciPice(Jelo p) {
+        narucenaJela.add(p);
+    }
+
+    void ispisiNarudzbinu() {
+        for (Jelo jelo : narucenaJela) {
+            System.out.println(jelo.toString() + "\n");
+        }
+        for (Pice pice : narucenaPica) {
             System.out.println(pice.toString() + "\n");
         }
     }
 }
-class Racun{
+
+class Racun {
     private Narudzbina n;
     double ukupnaCena;
-    Racun(Jelo narucenaJela,Pice narucenaPica)
-    {
-        this.narucenaJela=narucenaJela;
-        this.narucenaPica=narucenaPica;
-    }
-    void ukupnaCena()
-    {
-        for (Jelo jelo : n.narucenaJela )
-        {
-            ukupnaCena+=jelo.getCena();
-        }
-        for (Pice pice : narucenaPica)
-        {
-            ukupnaCena+=pice.getCena();
-        }
-    }
-    void ispisiRacun()
-    {
-        n.ispisiNarudzbinu();
-        System.out.println("Cena: "+ukupnaCena);
-    }
-}
-public class NemaNamirnice extends Exception {
 
-	public NemaNamirnice(String poruka) {
-		super(poruka);
-	}
-	
-}
-public static void main(String[] args) {
-    ArrayList<Namirnica> frizider=new ArrayList<Namirnica>();
-    BufferedReader br=new BufferedReader(new FileReader("frizider.txt"));
-    String linija=br.readLine();
-    String tokeni[]=linija.split(", ");
-    for(int i=0;i<tokeni.length;i++)
-    {
-        Namirnica n=new Namirnica(tokeni[i].trim());
-        frizider.add(n);
+    void ukupnaCena() {
+        for (Jelo jelo : n.narucenaJela) {
+            ukupnaCena += jelo.getCena();
+        }
+        for (Pice pice : n.narucenaPica) {
+            ukupnaCena += pice.getCena();
+        }
     }
-    br.close();
-    Meni meni=new Meni("jelovnik.txt");
-    meni.ispisiJela();
+
+    void ispisiRacun() {
+        n.ispisiNarudzbinu();
+        System.out.println("Cena: " + ukupnaCena);
+    }
+}
+
+public class Restoran {
+    static int opcije() {
+        Scanner s = new Scanner(System.in);
+        int opcija;
+        do {
+            System.out.println("\nIzaberite zeljenu operaciju:\n");
+            System.out.println(" 1. Naruciti obrok");
+            System.out.println(" 2. Racun");
+            System.out.println(" 3. Dovidjenja");
+            opcija = s.nextInt();
+            if (opcija < 1 || opcija > 3)
+                System.out.println("\nOpcija van opsega (1-3)! Izaberite zeljenu operaciju ponovo!\n");
+        } while (opcija > 1 || opcija < 3);
+        s.close();
+        return opcija;
+    }
+
+    static int opcije2() {
+        Scanner s = new Scanner(System.in);
+        int opcija2;
+        do {
+            System.out.println("\nIzaberite zeljenu operaciju:\n");
+            System.out.println(" 1. Naruci jelo");
+            System.out.println(" 2. Naruci pice");
+            System.out.println(" 3. Nastavi na racun");
+            opcija2 = s.nextInt();
+            if (opcija2 < 1 || opcija2 > 3)
+                System.out.println("\nOpcija van opsega (1-3)! Izaberite zeljenu operaciju ponovo!\n");
+        } while (opcija2 > 1 || opcija2 < 3);
+        s.close();
+        return opcija2;
+    }
+
+    public static void main(String[] args) {
+
+        int op; /// broj operacija
+        int op2;
+        System.out.println(
+                "-----------------------------------------------------------------------------------------------------------------------");
+        System.out.println("\t\t\t\t\t  DOBRODOSLI U RESTORAN");
+        System.out.println(
+                "-----------------------------------------------------------------------------------------------------------------------");
+
+        do {
+            op = opcije();
+            switch (op) {
+                case 1:
+                    do {
+                        op2 = opcije2();
+                        switch (op2) {
+                            case 1:
+                                break;
+
+                            case 2:
+                                break;
+                        }
+                    } while (op < 3);
+                    break;
+                case 2:
+                    break;
+
+            }
+        } while (op < 3);
+    }
 }
